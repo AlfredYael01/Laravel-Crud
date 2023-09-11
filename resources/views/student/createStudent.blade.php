34ac2468d5d69f595ff81559b792a84e9a75e376
Camille Marion <camille.marion@etu.iut-tlse3.fr>
2023-09-11 11:02:28 +0200

    page ajout élèves html

--- resources/views/student/createStudent.blade.php
+++ resources/views/student/createStudent.blade.php
@@ -1,11 +1,24 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <form>
-        <label for="name">Entrez votre nom</label>
-        <input type="text" name="name">
-        <label for="email">Entrez votre mail</label>
-        <input type="text" name="email">
+        <div class="name">
+            <label for="name">Entrez votre nom</label>
+            <input type="text" name="name">
+        </div>
+        <div class="email">
+            <label for="email">Entrez votre mail</label>
+            <input type="text" name="email">
+        </div>
+        <div class="form-example">
+            <input type="submit" value="Envoyer" />
+            <input type="submit" value="Supprimer" />
+        </div>
     </form>
+
+    <p> <h4> Affichage liste élèves </h4></p>
+    @foreach($Eleve as $eleve)
+        <p> {{$eleve->name}}  {{$eleve->surname}}</p>
+    @endforeach
 </html>
 
 
