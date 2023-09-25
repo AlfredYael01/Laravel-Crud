c6601fe28ef7e2cdf154e3a230fb44235869f203
Yael <yael.alfred@etu.iut-tlse3.fr>
2023-09-25 12:18:11 +0200

    Update EvaluationEleve Yael Alfred

--- app/Http/Controllers/EvaluationEleveController.php
+++ app/Http/Controllers/EvaluationEleveController.php
@@ -3,15 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\EvaluationEleve;
 class EvaluationEleveController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function evaluationEleve()
     {
-        //
+        $EvaluationEleve= EvaluationEleve::all();
+        return view('student.displayEvaluationNotes', compact('EvaluationEleve'));
     }
 
     /**
