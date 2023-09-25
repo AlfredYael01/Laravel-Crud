3466e2a0ceb18e816e1cb2aa3629fe4f9fdb8d90
Yael <yael.alfred@etu.iut-tlse3.fr>
2023-09-25 10:53:53 +0200

    Create Migration Yael Alfred

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 
+use App\Http\Controllers\Evaluation;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
---
+++ database/migrations/2023_09_25_084030_create_evaluation_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('evaluation', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->string('relation_mod');
+            $table->string('title');
+            $table->integer('coefficients');
+            $table->foreign('id')->references('id')->on('module');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('evaluation');
+    }
+};
