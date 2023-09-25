d67d9f72bf6dddf1526e00876d04a4a0ec46d33d
Camille Marion <camille.marion@etu.iut-tlse3.fr>
2023-09-25 10:35:05 +0200

    begining evaluation

--- app/Http/Controllers/EvaluationEleveController.php
+++ app/Http/Controllers/EvaluationEleveController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class EvaluationEleve extends Controller
+class EvaluationEleveController extends Controller
 {
     /**
      * Display a listing of the resource.
